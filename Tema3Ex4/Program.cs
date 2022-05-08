6f4dd8f1e6117753feca1713a83fe57aece0ad8d
Gergoa123 <gergo3124@gmail.com>
2022-05-08 15:42:08 +0300

    Add project files.

---
+++ Tema3Ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tema3Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0;
+            int n;
+            Console.WriteLine("introduceti numere");
+            n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                m += n;
+                n = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("suma numerelor este " + m  );
+        }
+    }
+}
